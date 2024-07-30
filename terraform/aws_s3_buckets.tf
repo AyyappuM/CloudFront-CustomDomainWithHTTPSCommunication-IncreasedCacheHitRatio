118c0e70167cfae2dd2001c21e129e5b10bb10ba
user <user@ubuntu-vm.myguest.virtualbox.org>
2024-07-30 16:23:14 +0530

    Access the object publicly from browser

--- terraform/aws_s3_buckets.tf
+++ terraform/aws_s3_buckets.tf
@@ -1,34 +1,44 @@
-resource "aws_s3_bucket" "Sample-S3-Bucket" {
+resource "aws_s3_bucket" "example" {
   bucket = "sample-bucket-${var.account_number}"
 }
 
 resource "aws_s3_object" "sample_s3_object" {
-  depends_on = [aws_s3_bucket.Sample-S3-Bucket]
+  depends_on = [aws_s3_bucket.example]
 
   bucket = "sample-bucket-${var.account_number}"
   key    = "MickeyMouse.jpg"
   source = "../files/MickeyMouse.jpg"
-}
-
-resource "aws_s3_bucket_ownership_controls" "Sample-S3-Bucket-Ownership-Control" {
-  bucket = aws_s3_bucket.Sample-S3-Bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "Sample-S3-Bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.Sample-S3-Bucket-Ownership-Control]
-
-  bucket = aws_s3_bucket.Sample-S3-Bucket.id
-  acl    = "public-read"
+  content_type = "image/jpeg" # The metadata will have content-type = application/octet-stream otherwise. The image/jpeg content-type is required to view the image in browser
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.Sample-S3-Bucket.id
+  bucket = aws_s3_bucket.example.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true  
+  ignore_public_acls      = true
+  block_public_policy     = false # Bucket policy can't be added without this
+  restrict_public_buckets = false # Object can't be publicly accessed, as defined by the bucket policy below, without this
+}
+
+resource "aws_s3_bucket_policy" "example" {
+    bucket = aws_s3_bucket.example.id
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::sample-bucket-${var.account_number}",
+          "arn:aws:s3:::sample-bucket-${var.account_number}/MickeyMouse.jpg"
+        ]
+      },
+    ]
+  })
+  
+  depends_on = [aws_s3_bucket_public_access_block.example]
 }
